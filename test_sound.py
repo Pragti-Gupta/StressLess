9a39fd41718ddd93305b4590dcaef513fc7486a5
nishi-fishi <nishi45016@gmail.com>
2026-04-11 23:15:44 -0700

    test_sound.py

--- test_sound.py
+++ test_sound.py
@@ -1,7 +1,7 @@
 from playsound import playsound
 
 try:
-    playsound('alert.mp3')
-    print("alert.mp3 played successfully")
+    playsound('alert.mov')
+    print("alert.mov played successfully")
 except Exception as e:
-    print(f"Error playing alert.mp3: {e}")
+    print(f"Error playing alert.mov: {e}")
